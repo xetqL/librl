8a8310d5700c9eb6eb323542f1b82d1ff466eabc
Anthony Boulmier <anthony.boulmier@hesge.ch>
2018-02-22 11:23:00 +0100

    .gitignore is now working

--- CMakeFiles/gridworld.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/gridworld"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/gridworld.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
