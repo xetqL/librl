1321d028ceebf2dfa5e4c3fdc50b83ad7dea68e7
Anthony Boulmier <anthony.boulmier@hesge.ch>
2018-03-08 16:16:40 +0100

    def

--- includes/librl/agents/RLAgentFactory.hpp
+++ includes/librl/agents/RLAgentFactory.hpp
@@ -11,6 +11,7 @@
 #include <vector>
 #include <exception>
 namespace librl { namespace agent {
+#if __cplusplus>201103L
 template<typename State, typename Action>
 class RLAgentFactory
 {
@@ -59,5 +60,55 @@ public:
         throw new std::invalid_argument("Not a valid algorithm/parameters combination.");
     }
 };
+#else
+template<typename State, typename Action>
+class RLAgentFactory
+{
+public:
+    static std::shared_ptr<RLAgent<State, Action>>
+    get_instance(std::string algorithm_name,
+                 double discount_factor,
+                 librl::policy::Policy<State, Action>* policy,
+                 librl::approximator::ActionValueApproximator<State, Action>* fa) {
+        if (!algorithm_name.compare("qlearning")) {
+            return std::make_shared<QLearning<State, Action>>(policy, fa, discount_factor);
+        } else if (!algorithm_name.compare("sarsa")) {
+            return std::make_shared<Sarsa<State, Action>>(policy, fa, discount_factor);
+        } else if (!algorithm_name.compare("expected-sarsa")) {
+            return std::make_shared<ExpectedSarsa<State, Action>>(policy, fa, discount_factor);
+        } else {
+            std::cerr << "This RL algorithm does not work with action value function approximator" << std::endl;
+        }
+        throw new std::invalid_argument("Not a valid algorithm/parameters combination.");
+    }
+
+    static std::shared_ptr<RLAgent<State, Action>>
+    get_instance(std::string algorithm_name,
+                 double discount_factor,
+                 librl::policy::Policy<State, Action>* policy,
+                 librl::approximator::DoubleApproximator<State, Action>* fa){
+        if (!algorithm_name.compare("double-qlearning")) {
+            return std::make_shared<DoubleQLearning<State, Action>>(policy, fa, discount_factor);
+        } else {
+            librl::approximator::ActionValueApproximator<State, Action>* generic_fa = fa;
+            return RLAgentFactory::get_instance(algorithm_name, discount_factor, policy, generic_fa);
+        }
+    }
+
+    static std::shared_ptr<RLAgent<State, Action>>
+    get_instance(std::string algorithm_name,
+                 double discount_factor,
+                 librl::policy::Policy<State, Action>* policy,
+                 librl::approximator::ActionValueApproximator<State,Action>* fa,
+                 librl::approximator::StateValueApproximator<State>* sfa){
+        if (!algorithm_name.compare("qvlearning")) {
+            return std::make_shared<QVLearning<State, Action>>(policy, fa, sfa, discount_factor);
+        } else {
+            std::cerr << "This RL algorithm does not work with state value function approximator" << std::endl;
+        }
+        throw new std::invalid_argument("Not a valid algorithm/parameters combination.");
+    }
+};
+#endif
 }}
 #endif
